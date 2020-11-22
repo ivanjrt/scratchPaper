5645d69fdb2967a561fff2661ea399558d8cbbbf
ivanjrt <44326428+ivanjrt@users.noreply.github.com>
2020-11-21 19:54:35 -0600

    Update scratchfile.ps1

--- scratchfile.ps1
+++ scratchfile.ps1
@@ -1,3 +1,4 @@
+md c:\temp
 $url = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-windows/azuredeploy.json"
 $output = "c:\Temp\template.json"
 Invoke-WebRequest -Uri $url -OutFile $output -verbose -UseBasicParsing
